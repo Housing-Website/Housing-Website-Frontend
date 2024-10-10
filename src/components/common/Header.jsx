ae186c28994770707569334c30927ae13d5b0a16
jin <dmlwls7094@naver.com>
2024-10-10 11:36:07 +0900

    Design: 네비바로 변경, slider 형태추가

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,23 +1,16 @@
-import React, { useState } from 'react';
-import { FaPhone } from "react-icons/fa6";
-import { LuMenu } from "react-icons/lu";
-import './style/Header.css';
-import Menu from '../Menu.jsx';
+import "./style/Header.css";
+import Menu from "../Menu.jsx";
+import { useState } from "react";
 
 function Header() {
   const [isMenuVisible, setIsMenuVisible] = useState(false);
 
   const toggleMenu = () => {
-    setIsMenuVisible(prev => !prev); 
+    setIsMenuVisible((prev) => !prev);
   };
 
   return (
     <>
-      <div className='main-header'>
-        <span className='main-title'>신광교 클라우드시티</span>
-        <span className='main-phone'><FaPhone className='call-icon' />1533.8389</span>
-        <LuMenu className='menu-icon' onClick={toggleMenu} />
-      </div>
       <Menu isVisible={isMenuVisible} toggleMenu={toggleMenu} />
     </>
   );
