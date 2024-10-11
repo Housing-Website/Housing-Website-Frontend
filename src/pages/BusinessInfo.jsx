eff93c7ca4d17f0e2697acffb515e268fc613891
subinsong01 <haely97@naver.com>
2024-10-11 15:57:46 +0900

    design: css business info

--- src/pages/BusinessInfo.jsx
+++ src/pages/BusinessInfo.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import './styles/BusinessInfo.css';
+import business1 from "../assets/images/business1.png";
+import business4 from "../assets/images/business4.png";
+import business5 from "../assets/images/business5.png";
 
 function BusinessInfo() {
   return (
-    <div>
-      사업정보
+    <div className='business-container'>
+      <div className='business-sub-container'>
+        <h2 className='business-title'>사업정보</h2>
+      </div>
+      <span className='business-subtitle'>비즈니스허브의탄생!<br />
+      <span className='business-subtitle-hightlight'>약 축구장48배, 여의도63빌딩2배규모, 연면적약35만m²,5개동,33층의 비즈니스랜드마크!</span>
+      </span>
+      <div className='business-section1'>
+        <img src={business1} alt='business1-img' className='business1-img'/>
+        <img src={business4} alt='business4-img' className='business4-img'/>
+      </div>
+      <div className='business-section2'>
+        <img src={business5} alt='business5-img' className='business5-img'/>
+      </div>
     </div>
   )
 }
