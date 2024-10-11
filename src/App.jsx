2a162ebddedad33a9524f50441f35f85b1f1f026
subinsong01 <haely97@naver.com>
2024-10-11 09:46:37 +0900

    design: 단지정보 메뉴 부분 css

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import MainPage from "./pages/MainPage";
 import BusinessInfo from "./pages/BusinessInfo";
 import Location from "./pages/Location";
 import Community from "./pages/Community";
-import ComplexInfo from "./pages/ComplexInfo";
 import Counsel from "./pages/Counsel";
+import ApartmentInfo from "./pages/ApartmentInfo";
 
 function App() {
   return (
@@ -18,7 +18,7 @@ function App() {
         <Route path="/Counsel" element={<Counsel />} />
         <Route path="/location" element={<Location />} />
         <Route path="/community" element={<Community />} />
-        <Route path="/complex-info" element={<ComplexInfo />} />
+        <Route path="/apartment-info" element={<ApartmentInfo />} />
       </Routes>
       <Footer />
     </BrowserRouter>
---
+++ src/pages/ApartmentInfo.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './styles/ApartmentInfo.css';
+
+function ApartmentInfo() {
+  return (
+    <div className='apt-container'>
+      <div className='apt-sub-container'>
+        <h2 className='apt-title'>단지정보</h2>
+      </div>
+    </div>
+  )
+}
+
+export default ApartmentInfo
