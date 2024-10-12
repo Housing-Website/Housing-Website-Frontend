8f32dad576603d1ee4c8161de6163599fa615399
subinsong01 <haely97@naver.com>
2024-10-12 22:59:23 +0900

    refactor: 라우터 설정 변경 및 예약현황페이지 css

--- src/pages/Booking.jsx
+++ src/pages/Booking.jsx
@@ -1,13 +1,29 @@
-/*로그인 했을 때 보이는 방문기록 페이지 > 관리자한테 보임*/
-
-import React from 'react'
+import React from 'react';
+import './styles/Booking.css';
 
 function Booking() {
   return (
-    <div>
-      방문페이지 입니다
+    <div className="booking-container">
+      <div className="booking-sub-container">
+        <h2 className="booking-title">예약현황</h2>
+      </div>
+      <div className='booking-list-container'>
+        <table className="booking-table">
+          <thead>
+            <tr>
+              <th>이름</th>
+              <th>전화번호</th>
+              <th>방문 날짜</th>
+              <th>메시지</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/*아직 없음*/}
+          </tbody>
+        </table>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Booking
+export default Booking;
