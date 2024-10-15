d9b1ec5d73787b1ab7c90facade7de7032b9724f
jin <dmlwls7094@naver.com>
2024-10-15 21:06:21 +0900

    eslint 오류 해결

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,24 +1,24 @@
+import PropTypes from "prop-types";
 import "./style/Footer.css";
 import CloudCityLogo from "../../assets/images/CloudCityLogo.jpeg";
 import { useNavigate } from "react-router-dom";
 
-function Footer({ isLoggedIn, setIsLoggedIn }) { 
+function Footer({ isLoggedIn, setIsLoggedIn }) {
   const navigate = useNavigate();
 
   const handleLoginPage = () => {
     if (isLoggedIn) {
-      setIsLoggedIn(false); 
-      sessionStorage.clear(); 
-      alert('로그아웃 되었습니다.');
-      navigate('/'); 
+      setIsLoggedIn(false);
+      sessionStorage.clear();
+      alert("로그아웃 되었습니다.");
+      navigate("/");
     } else {
-      navigate('/로그인페이지'); 
+      navigate("/로그인페이지");
     }
   };
 
   return (
     <div className="footer-container">
-      {/* 로고 이미지 추가 */}
       <div className="logo-container">
         <img
           src={CloudCityLogo}
@@ -26,7 +26,7 @@ function Footer({ isLoggedIn, setIsLoggedIn }) {
           className="footer-logo"
         />
         <button className="footer-login-btn" onClick={handleLoginPage}>
-          {isLoggedIn ? "로그아웃" : "관리자 로그인"} 
+          {isLoggedIn ? "로그아웃" : "관리자 로그인"}
         </button>
       </div>
       <div className="footer-info">
@@ -36,20 +36,24 @@ function Footer({ isLoggedIn, setIsLoggedIn }) {
         <span className="footer-infos">Tel : 1533 - 8389</span>
         <div className="footer-company-info">
           <span className="footer-company-infos">
-            상호명 : 신광교클라우드시티(대표) <br/>
-            시행: 기세(주) | 시공: 현대엔지니어링(주) <br/>
-            시행: 인창건설(주) | 시공: 현대엔지니어링(주) <br/>
+            상호명 : 신광교클라우드시티(대표) <br />
             ©2024 All Rights Reserved.
           </span>
           <span className="footer-disclaimer">
             ※ 본 웹사이트에 기재된 사업계획은 인•허가 과정에서 일부 변경될 수
-            있으며<br /> 사용된 CG 및 이미지는 소비자의 이해를 돕기 위한
-            것이며 실제와 다소 차이가 있을 수 있습니다.
+            있으며
+            <br /> 사용된 CG 및 이미지는 소비자의 이해를 돕기 위한 것이며 실제와
+            다소 차이가 있을 수 있습니다.
           </span>
         </div>
       </div>
     </div>
   );
 }
+
+Footer.propTypes = {
+  isLoggedIn: PropTypes.bool.isRequired,
+  setIsLoggedIn: PropTypes.func.isRequired,
+};
 
 export default Footer;
